ac0d308752f8eb6dda9de9e43a81b5fb1b7045ea
403247-coding <403247@aaps.k12.mi.us>
2026-03-11 18:51:32 -0400

    worked on limelight sensors

--- src/main/java/frc/robot/subsystems/drive/AlignmentSensors.java
+++ src/main/java/frc/robot/subsystems/drive/AlignmentSensors.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.signals.UpdateModeValue;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -12,8 +13,11 @@ public class AlignmentSensors extends SubsystemBase{
 
     private final CANrange rightCanrange = new CANrange(61);//change CAN ID later
     private final CANrange leftCanrange = new CANrange(60); //change CAN ID later
-
-    public String alignmentState = "not aligned";
+//CHANGE DEPENDING ON ALLIANCE BOTS!! our middle to end of intake is 2 ft
+    private final double distanceBetweenRobots = 5;//ft
+    //CHANGE LATER
+    private final double distanceBetweenCANranges = 2;//ft
+    public boolean isAligned = false;
 
     public AlignmentSensors(){
 
@@ -26,18 +30,21 @@ public class AlignmentSensors extends SubsystemBase{
 
         leftCanrange.getConfigurator().apply(canrangeConfig);
         rightCanrange.getConfigurator().apply(canrangeConfig);
-    }{
-
-        Boolean isAligned;
-        isAligned = rightCanrange.getIsDetected().getValue() && leftCanrange.getIsDetected().getValue();
-       
-
-    if (isAligned == true) {
-     alignmentState = "aligned";  
-    } else{
-        alignmentState = "not aligned";
     }
-}
+    public boolean isAligned(){
+        isAligned = rightCanrange.getIsDetected().getValue() && leftCanrange.getIsDetected().getValue();
+        return isAligned;
+    }
+    public ChassisSpeeds getAlignOutputs(){
+        double leftDistance = leftCanrange.getDistance().getValueAsDouble();//both meters
+        double rightDistance = rightCanrange.getDistance().getValueAsDouble();
+        leftDistance -= Units.feetToMeters(distanceBetweenRobots);
+        rightDistance -= Units.feetToMeters(distanceBetweenRobots);
+        double rotation = rightDistance - leftDistance;
+        rotation = Units.radiansToDegrees(Math.atan(rotation/distanceBetweenCANranges));
+        double x = (leftDistance + rightDistance)/2;
+        
+    }
 
   
 
